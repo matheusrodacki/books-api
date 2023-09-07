f63e267a6719d80af2d646e23250bdea5ecd5b13
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-07 10:35:13 -0300

    Add CRUD Methods

--- src/app.js
+++ src/app.js
@@ -4,29 +4,76 @@ const app = express();
 app.use(express.json());
 
 const livros = [
-    {
-        id: 1,
-        título: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        título: "O Hobbit"
-    }
+  {
+    id: 1,
+    título: "O Senhor dos Anéis",
+  },
+  {
+    id: 2,
+    título: "O Hobbit",
+  },
 ];
 
-
 app.get("/", (req, res) => {
-    res.status(200).send("***Curso de Node.JS***");
+  res.status(200).send("***Curso de Node.JS***");
 });
 
 app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-  });
+  res.status(200).json(livros);
+});
 
+app.get("/livros/:id", (req, res) => {
+  const index = buscalivro(req.params.id);
+
+  if (index != -1) {
+    res.status(200).json(livros[index]);
+  } else {
+    res.status(404).send("Erro! Livro não encontrado!");
+  }
+});
+
+function buscalivro(id) {
+  return livros.findIndex((livro) => {
+    return livro.id === Number(id);
+  });
+}
 
 app.post("/livros", (req, res) => {
+
+  const livro = req.body;
+
+  const index = buscalivro(livro.id);
+
+  if (index != -1) {
+    res.status(400).send("Erro! Esse livro já existe!");
+  } else {
     livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso!");
-}); 
+    res.status(200).send("Livro cadastrado com sucesso!");
+  }
+
+  
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscalivro(req.params.id);
+
+  if (index != -1) {
+    livros[index].título = req.body.título;
+    res.status(200).json(livros[index]);
+  } else {
+    res.status(404).send("Erro! Livro não encontrado!");
+  }
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscalivro(req.params.id);
+
+  if (index != -1) {
+    livros.splice(index, 1);
+    res.status(200).send("Livro apagado com sucesso!");
+  } else {
+    res.status(404).send("Erro! Livro não encontrado!");
+  }
+});
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -27,5 +27,5 @@ import app from "./app.js";
 const PORT = 4000;
 
 app.listen(PORT, () => {
-    console.log("Servidor escutando!")
-})
+  console.log("Servidor escutando!");
+});
