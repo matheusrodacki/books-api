ee0c2fed98b6c117d9cbe69775050fb1ef206316
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-07 05:49:34 -0300

    add app.js

---
+++ src/server.js
@@ -0,0 +1,31 @@
+//Server sem o Express:
+
+//import http from "http";
+
+//const PORT = 4000;
+//
+//const rotas = {
+//    "/": "Curso de Node.JS",
+//    "/teste" : "Teste OK! OK!",
+//    "/livros": "List Books",
+//    "/users": "List of Users!"
+// }
+//
+//const server = http.createServer((req, res) => {
+//    res.writeHead(200, { "Content-Type": "text/json"} );
+//    res.end(rotas[req.url]);
+//});
+//
+//server.listen(PORT, () => {
+//    console.log("Servidor escutando!")
+//})
+
+//Server com o Express:
+
+import app from "./app.js";
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log("Servidor escutando!")
+})
