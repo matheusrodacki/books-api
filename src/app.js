c7506bc3feb7978c93d387e7d1eb4e0ff45fff07
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-08 12:55:11 -0300

    refactoring for organization

--- src/app.js
+++ src/app.js
@@ -1,72 +1,18 @@
 import express from "express";
 import connectDB from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
+import routes from "./routes/index.js";
 
-const conection = await connectDB();
+const connection = await connectDB();
 
-conection.on("error", (erro) => {
+connection.on("error", (erro) => {
   console.error("DB Connection error!", erro);
 });
 
-conection.once("open", () => {
+connection.once("open", () => {
   console.log("Connection with DB it's Open...");
 });
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).send("***Curso de Node.JS***");
-});
-
-app.get("/livros", async (req, res) => {
-  const listaLivros = await livro.find({});
-  res.status(200).json(listaLivros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const index = buscalivro(req.params.id);
-
-  if (index != -1) {
-    res.status(200).json(livros[index]);
-  } else {
-    res.status(404).send("Erro! Livro não encontrado!");
-  }
-});
-
-app.post("/livros", (req, res) => {
-  const livro = req.body;
-
-  const index = buscalivro(livro.id);
-
-  if (index != -1) {
-    res.status(400).send("Erro! Esse livro já existe!");
-  } else {
-    livros.push(req.body);
-    res.status(200).send("Livro cadastrado com sucesso!");
-  }
-});
-
-app.put("/livros/:id", (req, res) => {
-  const index = buscalivro(req.params.id);
-
-  if (index != -1) {
-    livros[index].título = req.body.título;
-    res.status(200).json(livros[index]);
-  } else {
-    res.status(404).send("Erro! Livro não encontrado!");
-  }
-});
-
-app.delete("/livros/:id", (req, res) => {
-  const index = buscalivro(req.params.id);
-
-  if (index != -1) {
-    livros.splice(index, 1);
-    res.status(200).send("Livro apagado com sucesso!");
-  } else {
-    res.status(404).send("Erro! Livro não encontrado!");
-  }
-});
+routes(app);
 
 export default app;
