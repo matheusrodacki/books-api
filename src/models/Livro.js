630b12b93aa843db83c584121ffd4547ebd2aeee
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-11 10:28:28 -0300

    Fix typo and improve validation message in Livro model
    - Fix a typo in the validation message for the `editora` field in the Livro model.
    - Improve the validation for the `paginas` field in the Livro model by using a custom validator.

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -13,7 +13,7 @@ const livroSchema = new mongoose.Schema(
       required: [true, "O parâmetro `editora` é obrigatório"],
       enum: {
         values: ["Clássicos", "JK Publisher"],
-        message: "A editora '{VALUE}' não é um valor permitido!",
+        message: "A editora '{VALUE}' não é um valor !",
       },
     },
     preco: {
@@ -22,14 +22,13 @@ const livroSchema = new mongoose.Schema(
     },
     paginas: {
       type: Number,
-      min: [
-        10,
-        "O número de páginas deve estar entra 10 e 5000. Valor fornecido: '{VALUE}'",
-      ],
-      max: [
-        5000,
-        "O número de páginas deve estar entra 10 e 5000. Valor fornecido: '{VALUE}'",
-      ],
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message:
+          "O número de páginas deve estar entra 10 e 5000. Valor fornecido: '{VALUE}'",
+      },
     },
     autor: autorSchema,
   },
