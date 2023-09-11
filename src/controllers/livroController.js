403ac54e41c70333c4aa33cb8a744681b778264e
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-11 18:34:34 -0300

    refactor: Improve error handling in livroController and manipuladorDeErros
    - Improved error handling in livroController to handle incorrect requests using RequisicaoIncorreta error class.
    - Updated manipuladorDeErros middleware to handle errors derived from ErroBase class.

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,11 +1,27 @@
 import { livro, autor } from "../models/index.js";
 import NaoEncontrado from "../erros/NaoEncontrado.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 
 class LivroController {
   static async listarLivros(req, res, next) {
     try {
-      const listaLivros = await livro.find({});
-      res.status(200).json(listaLivros);
+      let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
+      limite = parseInt(limite);
+      pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
+
+      if (limite > 0 && pagina > 0) {
+        const listaLivros = await livro
+          .find({})
+          .sort({ [campoOrdenacao]: ordem })
+          .skip((pagina - 1) * limite)
+          .limit(limite);
+        res.status(200).json(listaLivros);
+      } else {
+        next(new RequisicaoIncorreta());
+      }
     } catch (erro) {
       next(erro);
     }
--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 import ErroBase from "../erros/ErroBase.js";
 import ErroValidacao from "../erros/ErroValidacao.js";
-import NaoEncontrado from "../erros/NaoEncontrado.js";
 import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 
 // eslint-disable-next-line no-unused-vars
-function manipuladorDeErros (erro, req, res, next) {
+function manipuladorDeErros(erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
     new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
     new ErroValidacao(erro).enviarResposta(res);
-  } else if (erro instanceof NaoEncontrado) {
+  } else if (erro instanceof ErroBase) {
     erro.enviarResposta(res);
   } else {
     new ErroBase().enviarResposta(res);
