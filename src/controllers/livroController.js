e210c983263a7818896a24f0760d741d742290e4
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-11 19:00:18 -0300

    feat: Update autor and livro controllers and add paginar middleware
    This commit updates the autor controller by modifying the endpoint '/autores' to add the paginar middleware. It also updates the livro controller by modifying the endpoints '/livros' and '/livros/busca' to add the paginar middleware. Additionally, a new 'paginar' middleware is added to handle pagination logic.

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,27 +1,12 @@
 import { livro, autor } from "../models/index.js";
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 
 class LivroController {
   static async listarLivros(req, res, next) {
     try {
-      let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
-
-      let [campoOrdenacao, ordem] = ordenacao.split(":");
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      ordem = parseInt(ordem);
-
-      if (limite > 0 && pagina > 0) {
-        const listaLivros = await livro
-          .find({})
-          .sort({ [campoOrdenacao]: ordem })
-          .skip((pagina - 1) * limite)
-          .limit(limite);
-        res.status(200).json(listaLivros);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
+      const buscaLivros = livro.find();
+      req.resultado = buscaLivros;
+      next();
     } catch (erro) {
       next(erro);
     }
@@ -93,9 +78,10 @@ class LivroController {
     try {
       const busca = await processaBusca(req.query);
 
-      const livrosBuscados = await livro.find(busca);
-      if (livrosBuscados.length > 0 && busca !== null) {
-        res.status(200).json(livrosBuscados);
+      if (busca !== null) {
+        const livrosBuscados = livro.find(busca);
+        req.resultado = livrosBuscados;
+        next();
       } else {
         res.status(200).send([]);
       }
