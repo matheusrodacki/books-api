1d3df619612bc194af0ce05e6d8c2bc7e5dbe9cb
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-11 12:52:33 -0300

    Refactor validator function for global validation in 'validadorGlobal.js'
    - Update the validator function in 'validadorGlobal.js' to check if the value is empty after trimming.
    - Modify the error message to indicate that the field is empty.
    
    This commit improves the global validation functionality by ensuring that empty fields are detected and providing a clear error message.

--- src/models/validadorGlobal.js
+++ src/models/validadorGlobal.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 mongoose.Schema.Types.String.set("validate", {
-  validator: (valor) => valor !== "",
-  message: ({ path }) => `O campo '${path}' está vazio`,
+  validator: (valor) => valor.trim() !== "",
+  message: ({ path }) => `O campo '${path}' está vazio!`,
 });
